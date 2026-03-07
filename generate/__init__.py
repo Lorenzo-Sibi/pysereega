5b5c637a5aea69ee80f9b4f531c0b66001b01bf0
Lorenzo Sibi <lorenzosibi2001@gmail.com>
2026-03-07 13:04:03 +0100

    Generate submodule branch. Initial commit

---
+++ generate/__init__.py
@@ -0,0 +1,17 @@
+"""
+Generate module - EEG data generation
+
+This module combines brain sources (location + signal + orientation) with
+forward models (lead fields) to generate realistic simulated EEG data.
+
+Core Classes:
+
+
+Core Functions:
+
+Quick start:
+"""
+
+__all__ = [
+    
+]
