c96a4cc3205d030ed07981e6bfe3911ba207611e
Lorenzo Sibi <lorenzosibi2001@gmail.com>
2026-03-07 12:52:13 +0100

    Add implementation of the class SignalComposition: now it is possible to compose multiple signal into a single one. Add plot funcitonality for studying the single components' shape, deviations and per-epoch-change

---
+++ signal/__init__.py
@@ -0,0 +1,10 @@
+from .signal import Signal, SignalComposition
+from .erp import ERPSignal
+from .noise import NoiseSignal
+
+__all__ = [
+    'Signal',
+    'SignalComposition',
+    'ERPSignal',
+    'NoiseSignal',
+]
